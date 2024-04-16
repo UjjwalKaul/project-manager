08b655ffca622b5bbdf4f04d06dde40e5ed66f22
Ujjawal Kaul <ujjwal171@outlook.com>
2024-04-17 00:22:01 +0530

    Added default screen

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Input from './Input'
 
 export default function NewProject() {
+
     return <div className='w-[35rem] mt-16'>
         <menu className='flex items-center justify-end gap-4 my-4'>
             <li><button className='text-stone-700 hover:text-stone-950'>Cancel</button></li>
