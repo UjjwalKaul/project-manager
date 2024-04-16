ecf132fbb2f46c6573d58c76e6f775409f08e6f4
Ujjawal Kaul <ujjwal171@outlook.com>
2024-04-17 02:52:36 +0530

    Connected State to form inputs

--- src/App.jsx
+++ src/App.jsx
@@ -9,20 +9,32 @@ function App() {
     projects: []
   })
 
-  function handleAddProject() {
+  function handleStartAddProject() {
     setProjectsState((prevState) => { return { ...prevState, selectedProjectId: null } })
   }
 
+  function handleAddProject(projectData) {
+    setProjectsState((prevState) => {
+      const newProject = {
+        ...projectData,
+        id: Math.random()
+      }
+      return { ...prevState, projects: [...prevState.projects, newProject] }
+    })
+  }
+
+  console.log(projectsState.projects)
+
   let content
   if (projectsState.selectedProjectId === null) {
-    content = <NewProject />
+    content = <NewProject onAdd={handleAddProject} />
   }
   else if (projectsState.selectedProjectId === undefined) {
-    content = <NoProjectAdded onStartAdd={handleAddProject} />
+    content = <NoProjectAdded onStartAdd={handleStartAddProject} />
   }
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar onStartAdd={handleAddProject} />
+      <ProjectsSidebar onStartAdd={handleStartAddProject} />
       {content}
     </main>
   );
--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,17 +1,32 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Input from './Input'
 
-export default function NewProject() {
+export default function NewProject({ onAdd }) {
+    const title = useRef()
+    const desc = useRef()
+    const due = useRef()
+
+    function handleSave() {
+        const enteredTitle = title.current.value
+        const enteredDesc = desc.current.value
+        const enteredDue = due.current.value
+        //Validation
+        onAdd({
+            title: enteredTitle,
+            desc: enteredDesc,
+            due: enteredDue
+        })
+    }
 
     return <div className='w-[35rem] mt-16'>
         <menu className='flex items-center justify-end gap-4 my-4'>
             <li><button className='text-stone-700 hover:text-stone-950'>Cancel</button></li>
-            <li><button className='rounded-md px-6 py-2 bg-stone-800 text-stone-50 hover:bg-stone-950'>Save</button></li>
+            <li><button onClick={handleSave} className='rounded-md px-6 py-2 bg-stone-800 text-stone-50 hover:bg-stone-950'>Save</button></li>
         </menu>
         <div>
-            <Input label={"Title"} />
-            <Input label={"Description"} isTextarea />
-            <Input label={"Due Date"} />
+            <Input type="text" label={"Title"} ref={title} />
+            <Input label={"Description"} isTextarea ref={desc} />
+            <Input type="date" label={"Due Date"} ref={due} />
         </div>
     </div>
 }
