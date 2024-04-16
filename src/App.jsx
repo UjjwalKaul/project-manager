57c12edf00ca3c72e7bb69f09e4f7f201a4f7d1b
Ujjawal Kaul <ujjwal171@outlook.com>
2024-04-17 02:07:53 +0530

    Added switching from no project to create new project

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject";
 import NoProjectAdded from "./components/NoProjectAdded";
 import ProjectsSidebar from "./components/ProjectsSidebar";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  })
+
+  function handleAddProject() {
+    setProjectsState((prevState) => { return { ...prevState, selectedProjectId: null } })
+  }
+
+  let content
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />
+  }
+  else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectAdded onStartAdd={handleAddProject} />
+  }
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar />
-      <NoProjectAdded />
-      {/* <NewProject /> */}
+      <ProjectsSidebar onStartAdd={handleAddProject} />
+      {content}
     </main>
   );
 }
